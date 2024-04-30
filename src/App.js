37cd3bbe1497db992e07d31376ba2aaab3e059b2
Narreddy Saini <narreddysaini1506@gmail.com>
2024-04-30 08:29:22 +0530

    creation of accordion faqs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
+import data from './data.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    activeIndex : null
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <center>
+          <ul>
+            {
+              data.map((question, active) => {
+                return (
+                  <li>
+                    <h2 onClick={() => {
+                      this.setState({activeIndex : this.state.activeIndex === active ? null : active})
+                    }} className="question">{question.Q}</h2>
+                   {active === this.state.activeIndex &&  <p className="answer">{question.A}</p>}
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </center>
+      </>
+    );
+  }
 }
 
 export default App;
